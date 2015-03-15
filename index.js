f930d082e07fce09f2a4e8c1b74e4d1244ff0ab7
Harish Anchu <harishanchu@gmail.com>
2015-03-15 11:24:12 +0530

    Framework entry script added

---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * index.js
+ *
+ * @author: Harish Anchu <harishanchu@gmail.com>
+ * @copyright 2015, QuorraJS. All rights reserved.
+ * @license Licensed under MIT
+ */
+
+/*
+ |--------------------------------------------------------------------------
+ | Start the Engine
+ |--------------------------------------------------------------------------
+ |
+ | We need to enlighten  Node.js development, so let's start the engine,
+ | turn on the lights. This bootstraps the framework and gets it ready for use,
+ | then it will load up this application so that we can run it and send
+ | the responses back to the browser and delight these users.
+ |
+ */
+
+require('./bootstrap/start')(function(app){
+
+    /*
+     |--------------------------------------------------------------------------
+     | Listen for requests
+     |--------------------------------------------------------------------------
+     |
+     | Once we have the application, we can simply call the listen method,
+     | which will start to listen for user requests.
+     |
+     */
+
+    app.listen();
+});
