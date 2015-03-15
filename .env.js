aef744d65394739c782ce5c8a016d1ce14936c71
Harish Anchu <harishanchu@gmail.com>
2015-03-15 11:22:58 +0530

    environment conf files package.json and git ignore files are added

---
+++ .env.js
@@ -0,0 +1,13 @@
+/**
+ * .env.js
+ *
+ * @author: Harish Anchu <harishanchu@gmail.com>
+ * @copyright 2015, QuorraJS. All rights reserved.
+ * @license Licensed under MIT
+ */
+
+var config = {
+    secret: "my_secret"
+};
+
+module.exports = config;
