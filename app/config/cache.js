2f10e1409db004e0ceceec7776eba7d67f376314
Harish Anchu <harishanchu@gmail.com>
2015-03-24 18:45:16 +0530

    add cache config

---
+++ app/config/cache.js
@@ -0,0 +1,16 @@
+/**
+ * cache.js
+ *
+ * @author: Harish Anchu <harishanchu@gmail.com>
+ * @copyright 2015, QuorraJS. All rights reserved.
+ * @license Licensed under MIT
+ */
+
+var config = {
+
+    // true || weak || strong || false || fn
+    'etag': true
+
+};
+
+module.exports = config;
