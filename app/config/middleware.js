8c3fe4b94af31b2153bd02a44aa5c45f5c292f69
Harish Anchu <harishanchu@gmail.com>
2015-03-30 12:46:09 +0530

    add middleware configuration

---
+++ app/config/middleware.js
@@ -0,0 +1,15 @@
+/**
+ * middleware.js
+ *
+ * @author: Harish Anchu <harishanchu@gmail.com>
+ * @copyright 2015, QuorraJS. All rights reserved.
+ * @license Licensed under MIT
+ */
+
+var config = {
+    'queryParser': 'extended',
+    'bodyParser': true,
+    'serveStatic': true
+};
+
+module.exports = config;
