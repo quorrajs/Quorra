2ce60a1f545a55a9878b0f1833fb421e3a7f5826
Harish Anchu <harishanchu@gmail.com>
2015-04-01 13:00:22 +0530

    Add demo model

---
+++ app/models/User.js
@@ -0,0 +1,18 @@
+/**
+ * User.js
+ *
+ * @author: Harish Anchu <harishanchu@gmail.com>
+ * @copyright 2015, QuorraJS. All rights reserved.
+ * @license Licensed under MIT
+ */
+
+var User = {
+    attributes: {
+        firstName: 'string',
+        lastName: 'string'
+    },
+    migrate: 'drop'
+};
+
+module.exports = User;
+
