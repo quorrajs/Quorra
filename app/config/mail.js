5230d04b93764857c3896de2f605ba89b290f931
Harish <harishanchu@gmail.com>
2016-01-17 23:44:36 +0530

    Update mail config docs

--- app/config/mail.js
+++ app/config/mail.js
@@ -8,7 +8,28 @@
 
 var config = {
 
+    /*
+     |--------------------------------------------------------------------------
+     | Transport driver
+     |--------------------------------------------------------------------------
+     |
+     | Here you may specify nodemailer transport driver you wish to use. If you
+     | specify anything other  than the default driver you may have to install
+     | that driver manually using npm install command
+     | Find more drivers at https://github.com/nodemailer/nodemailer#send-using-a-transport-plugin
+     |
+     */
+
     'driver': 'nodemailer-smtp-transport',
+
+    /*
+     |--------------------------------------------------------------------------
+     | Transport driver config
+     |--------------------------------------------------------------------------
+     |
+     | Specify the configuration values for the driver you specified above.
+     |
+     */
 
     'driverConfig': {
         'service': 'gmail',
