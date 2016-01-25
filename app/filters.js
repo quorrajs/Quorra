6d00a6d9ff0c1099b2bc81d3be27791594c10810
Harish <harishanchu@gmail.com>
2016-01-25 17:12:41 +0530

    Add CSRF filter

--- app/filters.js
+++ app/filters.js
@@ -27,3 +27,23 @@ Route.filter('auth', function(request, response, next, value, value1) {
     //console.log(value1);
     next();
 });
+
+/*
+ |--------------------------------------------------------------------------
+ | CSRF Protection Filter
+ |--------------------------------------------------------------------------
+ |
+ | The CSRF filter is responsible for protecting your application against
+ | cross-site request forgery attacks. If this special token in a user
+ | session does not match the one given in this request, we'll bail.
+ |
+ */
+
+Route.filter('csrf', function(request, response, next) {
+    if (App.session.getToken() !== request.input.get('_token'))
+    {
+        throw new Error('Token mismatch');
+    } else {
+        next();
+    }
+});
