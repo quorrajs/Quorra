13e02bac176be5c130725eec6232ccba6fa38763
Harish <harishanchu@gmail.com>
2016-01-25 17:13:19 +0530

    Update js docs

--- app/config/middleware.js
+++ app/config/middleware.js
@@ -8,6 +8,26 @@
 
 var config = {
     'favicon': true,
+
+    /*
+     |--------------------------------------------------------------------------
+     | Query Parser
+     |--------------------------------------------------------------------------
+     |
+     | Disable query parsing by setting the value to false, or set the query
+     | parser to use either “simple” or “extended” or a custom query string
+     | parsing function.
+     |
+     | The simple query parser is based on Node’s native query parser,
+     | querystring.
+     |
+     | The extended query parser is based on qs.
+     |
+     | A custom query string parsing function will receive the complete query
+     | string, and must return an object of query keys and their values.
+     |
+     */
+
     'queryParser': 'extended',
     'bodyParser': true,
     'serveStatic': true,
