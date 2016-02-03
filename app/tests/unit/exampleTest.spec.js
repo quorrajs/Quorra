cf3e13a4d02902fd1d12d7f8e34bf67b07e72170
Harish <harishanchu@gmail.com>
2016-02-03 23:50:53 +0530

    Add sample test files

---
+++ app/tests/unit/exampleTest.spec.js
@@ -0,0 +1,15 @@
+
+describe('UserModel', function() {
+
+    describe('#find()', function() {
+        it('should check find function', function (done) {
+            User.find()
+                .then(function(results) {
+                    // some tests
+                    done();
+                })
+                .catch(done);
+        });
+    });
+
+});
