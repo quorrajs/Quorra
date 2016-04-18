2e5b35f9be48ba9a6057cefe46c277148a424a78
Harish Anchu <harishanchu@gmail.com>
2016-04-18 20:11:13 +0530

    Add auth configurations

---
+++ app/config/auth.js
@@ -0,0 +1,18 @@
+
+var config = {
+
+    /*
+     |--------------------------------------------------------------------------
+     | Authentication Model
+     |--------------------------------------------------------------------------
+     |
+     | Waterline models global id, that should be used for retrieving users
+     |
+     */
+
+    'model': 'user'
+
+
+};
+
+module.exports = config;
