7964df3e320d5e31805a222477e23b14023db788
Harish <harishanchu@gmail.com>
2016-04-19 22:38:45 +0530

    Update database config docs

--- app/config/globals.js
+++ app/config/globals.js
@@ -21,7 +21,7 @@ var config = {
      | Expose each of your app's models as global variables (using their
      | "globalId"). E.g. a model defined in app/models/User.js would have a
      | globalId of User by default. If this is disabled, you can still access
-     | your models via App.models.*.
+     | your models via App.models.<model identity>.
      |
      */
 
